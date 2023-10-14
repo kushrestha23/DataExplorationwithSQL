d0bb3d7bbd18d58b70e3274d7a01a20063ac23ae
kushrestha23 <kushrestha23@gmail.com>
2023-10-13 21:06:18 -0400

    Adding queries for the sales analysis based on the category

---
+++ SalesAnalysisBasedOnCategory.sql
@@ -0,0 +1,31 @@
+/* Analysing the sales */ --Selecting and observing the film actors
+
+SELECT *
+FROM FILM_ACTOR 
+
+--Selecting and observing the film table and ordering in the descending order.
+SELECT *
+FROM FILM
+ORDER BY FILM DESC;
+
+--Observing the film category table.
+SELECT *
+FROM FILM_CATEGORY
+SELECT*
+FROM CATEGORY 
+
+--Joining the film id between the film and fil_category to observe the data
+SELECT FILM.FILM_ID,
+	FILM_CATEGORY.CATEGORY_ID,
+	TITLE
+FROM FILM
+JOIN FILM_CATEGORY ON FILM.FILM_ID = FILM_CATEGORY.FILM_ID;
+
+-- Joining the film, film_category and category table for observation
+SELECT FILM.FILM_ID,
+	FILM_CATEGORY.CATEGORY_ID,
+	FILM.TITLE,
+	CATEGORY.NAME
+FROM FILM
+JOIN FILM_CATEGORY ON FILM.FILM_ID = FILM_CATEGORY.FILM_ID
+JOIN CATEGORY ON FILM_CATEGORY.CATEGORY_ID = CATEGORY.CATEGORY_ID;
